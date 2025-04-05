bcd80f47600a17f019bc42f8fea675bff64a39cb
PoojithaJagadam <162295791+PoojithaJagadam@users.noreply.github.com>
2025-04-05 10:53:43 +0530

    Time: 9 ms (94.72%), Space: 56.9 MB (53.1%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    public static ListNode merge(ListNode p1 , ListNode p2){
+        // ListNode p1=list1;
+        // ListNode p2=list2;
+        ListNode dummy=new ListNode(-1);
+        ListNode td=dummy;
+        while(p1!=null && p2!=null) {
+            if(p1.val<p2.val) {
+                dummy.next=p1;
+                p1=p1.next;
+                
+            }
+            else {                
+                dummy.next=p2;
+                p2=p2.next;
+               
+            }
+            dummy=dummy.next;
+        }
+       if(p1!=null) dummy.next=p1;
+        if(p2!=null) dummy.next=p2;
+        return td.next;
+    }
+
+    public static ListNode findMid(ListNode head) {
+        //ListNode temp=head;
+         ListNode slow=head,fast=head;
+         while(fast.next!= null && fast.next.next!=null) {   
+                 slow= slow.next;
+                 fast= fast.next.next;    
+         }
+         return slow;
+    }
+    public ListNode sortList(ListNode head) {
+        
+         if(head==null || head.next==null) {
+            return head;
+         }
+          ListNode mid=findMid(head);
+         ListNode sh=mid.next;
+         mid.next=null;
+         return merge(sortList(head),sortList(sh));
+    }
+}
