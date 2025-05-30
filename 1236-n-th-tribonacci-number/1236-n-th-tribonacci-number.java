33f38764cad8bce73f6de589eb4738140f193582
PoojithaJagadam <162295791+PoojithaJagadam@users.noreply.github.com>
2025-05-30 23:34:00 +0530

    Time: 0 ms (100%), Space: 40.8 MB (10.42%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -0,0 +1,23 @@
+class Solution {
+   /* static int fib() {
+        int[] dp=new int[38];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3;i<=37;i++) {
+            dp[i]= dp[i-1] + dp[i-2] +dp[i-3];
+        }
+    } */
+
+    public int tribonacci(int n) {
+       int[] dp=new int[38];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3;i<=37;i++) {
+            dp[i]= dp[i-1] + dp[i-2] +dp[i-3];
+        }
+        return dp[n];
+    }
+    
+}
