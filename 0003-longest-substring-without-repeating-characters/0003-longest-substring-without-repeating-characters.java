d2553c020af5d43fe5c3c776cb3b41c58cd98282
PoojithaJagadam <162295791+PoojithaJagadam@users.noreply.github.com>
2025-07-09 20:06:32 +0530

    Time: 7 ms (35.19%), Space: 44.8 MB (43.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -5,10 +5,12 @@ class Solution {
        int n=s.length();
         HashSet<Character> hash=new HashSet<>();
         for(int r=0;r<n;r++) {
+            //  if hashset doesnot contain the incoming character then add it
             if(!hash.contains(s.charAt(r))) {
                 hash.add(s.charAt(r));
             }
             else {
+                // otherwise remove that charcter which is at the starting and add the current character to the end 
                 while(hash.contains(s.charAt(r))) {
                     
                     hash.remove(s.charAt(l));
@@ -16,6 +18,7 @@ class Solution {
                 }
                 hash.add(s.charAt(r));
             }
+            // update the answer
             ans = Math.max(ans, r-l+1);
         }
         return ans;
