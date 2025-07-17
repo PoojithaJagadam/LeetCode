999e92bd9ae5a656daa732af77fabbe99bd2b3ed
PoojithaJagadam <162295791+PoojithaJagadam@users.noreply.github.com>
2025-07-17 21:08:16 +0530

    Time: 32 ms (95.88%), Space: 55 MB (12.37%) - LeetHub

--- 3491-find-the-maximum-length-of-valid-subsequence-ii/3491-find-the-maximum-length-of-valid-subsequence-ii.java
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/3491-find-the-maximum-length-of-valid-subsequence-ii.java
@@ -1,12 +1,16 @@
 class Solution {
     public int maximumLength(int[] nums, int k) {
         int n=nums.length;
+        // we used 2d array because to find all combinations of subsequences its leads to k*k combinations 
+        // starting dp is filled with 0's
         int[][] dp=new int[k][k];
         int ans=0;
         for(int num:nums) {
             num%=k;
             for(int i=0;i<k;i++) {
+                // if it present before thenincrement its length
                 dp[i][num] = dp[num][i] +1;
+                // update the ans to max value
                 ans= Math.max(ans, dp[i][num]);
             }
         }
