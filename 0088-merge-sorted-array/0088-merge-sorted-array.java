5768270842ed01cd7280709cc5b544002ded368e
PoojithaJagadam <162295791+PoojithaJagadam@users.noreply.github.com>
2025-07-18 10:30:46 +0530

    Time: 0 ms (100%), Space: 42.3 MB (42.33%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,10 +1,9 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
        int size=m+n;
-       int n1=nums1.length-n;
-       int n2=nums2.length;
        int[] c=new int[size+1];
-       
+    //    using two pointer
+    // in this we use extra space of size m+n 
        int p1=0, p2=0;
        int k=0;
        while(p1 < m && p2<n) {
@@ -15,16 +14,15 @@ class Solution {
                 c[k++]= nums2[p2++];
             }
        }
-       while(p1<n1) {
+       while(p1<m) {
             c[k++]= nums1[p1++];
        }
-       while(p2<n2) {
+       while(p2<n) {
         c[k++] = nums2[p2++];
        }
 
        for(int i=0;i<size;i++) {
             nums1[i]= c[i];
        }
-
     }
 }
