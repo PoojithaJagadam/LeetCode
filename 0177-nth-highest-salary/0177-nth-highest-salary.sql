aa071517ec5cb804b2450f38d04334ab07bfaa1f
PoojithaJagadam <162295791+PoojithaJagadam@users.noreply.github.com>
2025-08-18 22:17:01 +0530

    Time: 465 ms (33.38%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,8 +1,9 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-    SET N=N-1;
+    SET N=N-1; #offset is pointed to 0-based indexing set first we will set to n-1
   RETURN (
       # Write your MySQL query statement below.
         select distinct  salary from Employee order by salary desc limit 1 offset N
   );
+  # limit is used to display no. of records offset means to skip no.of records
 END
