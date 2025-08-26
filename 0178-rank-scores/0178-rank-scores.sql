0bc28dcb954351e11267308dcb412d80a8785b13
PoojithaJagadam <162295791+PoojithaJagadam@users.noreply.github.com>
2025-08-26 18:53:20 +0530

    Time: 297 ms (72.76%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,6 +1,11 @@
 # Write your MySQL query statement below
 
 #dense_rank() is a ranking function which gives same rank for same scores and continue with next rank
-select score , DENSE_RANK()
+/*select score , DENSE_RANK()
 OVER(order by score desc) as 'rank'
-from  Scores;
+from  Scores;*/
+
+SELECT score, DENSE_RANK()
+Over(order by score desc) as 'rank'
+from Scores
+
